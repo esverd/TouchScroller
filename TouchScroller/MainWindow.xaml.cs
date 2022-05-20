66871e7227861a3f4bc9d43a0313d5c8c53128b1
Sverd Industries <eirik.sverd@gmail.com>
2022-05-20 13:06:31 +0200

    getting click and coordinate of mouse inside wpf

--- TouchScroller/MainWindow.xaml.cs
+++ TouchScroller/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WindowsInput.Native;
 using WindowsInput;
+using System.Diagnostics;
+using WindowsHook;
 
 namespace TouchScroller
 {
@@ -23,18 +25,60 @@ namespace TouchScroller
     public partial class MainWindow : Window
     {
         InputSimulator simulator = new InputSimulator();
+        private IKeyboardMouseEvents m_GlobalHook;
+
         public MainWindow()
         {
             InitializeComponent();
+            Subscribe();
+
         }
 
 
         private void btnUp_Click(object sender, RoutedEventArgs e)
         {
-            
-            simulator.Mouse.MoveMouseTo(45000, 30000);
+
+            //simulator.Mouse.MoveMouseTo(45000, 30000);
             //simulator.Mouse.RightButtonClick();
-            simulator.Mouse.VerticalScroll(-10);
+            //simulator.Mouse.VerticalScroll(-10);
+            //Debug.WriteLine("Test!");
+            //MessageBox.Show("hello");
         }
+
+
+        public void Subscribe()
+        {
+            // Note: for the application hook, use the Hook.AppEvents() instead
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Debug.WriteLine("KeyPress: \t{0}", e.KeyChar);
+        }
+
+        private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
+        {
+            MessageBox.Show(e.Button.ToString() + " " + e.X + " " + e.Y);
+            
+            //Debug.WriteLine("MouseDown: \t{0}; \t System Timestamp: \t{1}", e.Button, e.Timestamp);
+
+            // uncommenting the following line will suppress the middle mouse button click
+            // if (e.Buttons == MouseButtons.Middle) { e.Handled = true; }
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
+            m_GlobalHook.KeyPress -= GlobalHookKeyPress;
+
+            //It is recommened to dispose it
+            m_GlobalHook.Dispose();
+        }
+
+
     }
 }

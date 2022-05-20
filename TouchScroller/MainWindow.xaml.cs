a702263fdaef4ba849b3ce65f839059010db8fb6
Sverd Industries <eirik.sverd@gmail.com>
2022-05-20 13:24:23 +0200

    cleanup

--- TouchScroller/MainWindow.xaml.cs
+++ TouchScroller/MainWindow.xaml.cs
@@ -24,25 +24,22 @@ namespace TouchScroller
     /// </summary>
     public partial class MainWindow : Window
     {
-        InputSimulator simulator = new InputSimulator();
+        private InputSimulator mouseSim;
         private IKeyboardMouseEvents m_GlobalHook;
 
         public MainWindow()
         {
             InitializeComponent();
+            mouseSim = new InputSimulator();
             Subscribe();
-
         }
 
 
         private void btnUp_Click(object sender, RoutedEventArgs e)
         {
-
             //simulator.Mouse.MoveMouseTo(45000, 30000);
             //simulator.Mouse.RightButtonClick();
             //simulator.Mouse.VerticalScroll(-10);
-            Debug.WriteLine("Test!");
-            //MessageBox.Show("hello");
         }
 
 
@@ -50,32 +47,18 @@ namespace TouchScroller
         {
             // Note: for the application hook, use the Hook.AppEvents() instead
             m_GlobalHook = Hook.GlobalEvents();
-
             m_GlobalHook.MouseDownExt += GlobalHookMouseDownExt;
-            m_GlobalHook.KeyPress += GlobalHookKeyPress;
         }
 
-        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
-        {
-            //Debug.WriteLine("KeyPress: \t{0}", e.KeyChar);
-        }
 
         private void GlobalHookMouseDownExt(object sender, MouseEventExtArgs e)
         {
-            //MessageBox.Show(e.Button.ToString() + " " + e.X + " " + e.Y);
             Debug.WriteLine(e.Button.ToString() + " " + e.X + " " + e.Y);
-            
-            //Debug.WriteLine("MouseDown: \t{0}; \t System Timestamp: \t{1}", e.Button, e.Timestamp);
-
-            // uncommenting the following line will suppress the middle mouse button click
-            // if (e.Buttons == MouseButtons.Middle) { e.Handled = true; }
         }
 
         public void Unsubscribe()
         {
             m_GlobalHook.MouseDownExt -= GlobalHookMouseDownExt;
-            m_GlobalHook.KeyPress -= GlobalHookKeyPress;
-
             //It is recommened to dispose it
             m_GlobalHook.Dispose();
         }
